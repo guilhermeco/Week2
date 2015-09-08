2af970d6b72d64c91d160d888c8d5dafbbbf5cbd
Guilherme Coelho <guilhermeismco@gmail.com>
2015-09-08 18:22:48 -0400

    pos change

---
+++ Week2 - Unity/Assets/PositionChanger.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PositionChanger : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log (gameObject.transform.position);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.transform.position = gameObject.transform.position + new Vector3(1, 0, 0);
+	}
+}
